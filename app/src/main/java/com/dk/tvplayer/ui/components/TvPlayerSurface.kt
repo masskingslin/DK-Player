c3c2a3536b2cbde776165c92f004d67c1f45ed42
Mass Kingslin <masskingslin@gmail.com>
2026-08-27 16:01:48 +0530

    Create components/TvPlayerSurface.kt

---
+++ app/src/main/java/com/dk/tvplayer/ui/components/TvPlayerSurface.kt
@@ -0,0 +1,33 @@
+package com.dk.tvplayer.ui.components
+
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+@OptIn(UnstableApi::class)
+@Composable
+fun TvPlayerSurface(
+    exoPlayer: ExoPlayer,
+    modifier: Modifier = Modifier
+) {
+    AndroidView(
+        factory = { context ->
+            PlayerView(context).apply {
+                this.player = exoPlayer
+                useController = false
+                layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+        },
+        modifier = modifier.fillMaxSize()
+    )
+}
