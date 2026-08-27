ceb5718e8743444c911ef50cec4436dcbf692264
Mass Kingslin <masskingslin@gmail.com>
2026-08-27 16:13:19 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,15 @@ android {
 }
 
 dependencies {
-    // Core & Lifecycle
+    // Core & Lifecycle KTX
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
-    // Jetpack Compose BOM & UI
+    // Jetpack Compose
     val composeBom = platform("androidx.compose:compose-bom:2024.08.00")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
